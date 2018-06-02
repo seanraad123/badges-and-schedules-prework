1b149739643f3de03aedb752c45c24e4857b071c
Sean Conrad <seanconrad123@gmail.com>
2018-06-02 06:23:32 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,9 @@ end
 
 def assign_rooms(list_of_speakers)
   rooms = [1, 2, 3, 4, 5, 6, 7]
-  print list_of_speakers.each_with_index()
+  print list_of_speakers.each_with_index() { |item, index|
+  hash[item] = index
+}
 end
 
 assign_rooms(names)
