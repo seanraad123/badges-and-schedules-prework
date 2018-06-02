ba5ffb7e4a4cee6f70dd334b1eceddffe8670e98
Sean Conrad <seanconrad123@gmail.com>
2018-06-02 06:11:34 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+
+names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array_of_names)
+  return "Badge messages"
+end
+
+def assign_rooms(list_of_speakers)
+  list_of_speakers.each_with_index()
